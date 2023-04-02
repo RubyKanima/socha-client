73ad19877c68e62db929db85973e134e2cb0178c
RubyKanima <100081870+RubyKanima@users.noreply.github.com>
2023-04-03 01:02:03 +0200

    Functional Alpha Beta Prune with Memory
    - MinMax as base concept
    - Alpha-Beta prune to reduce calculation time
    - Memory that memorizes which turns were already calculated apart from their order
    - Semi-relative depth

--- alpha-beta.py
+++ alpha-beta.py
@@ -1,17 +1,33 @@
 import random
 import logging
+import collections
+compare = lambda x, y: collections.Counter(x) == collections.Counter(y)
 from socha import *
 
 class Logic(IClientHandler):
     game_state: GameState
 
-    def min_max(self, new_state: GameState, depth: int, alpha, beta, memo = {}) -> int :
-            hash_list = set(new_state.first_team.moves + new_state.second_team.moves) # Move not hashable !FIX!
-            if hash_list in memo:
-                return memo[hash_list]
+    def move_hash(self, state: GameState):
+        move_list = []
+        for move in state.first_team.moves:
+            move_str = str(move.to_value.x)+str(move.to_value.y)+str(move.team_enum.name)
+            move_list.append(move_str)
+        for move in state.second_team.moves:
+            move_str = str(move.to_value.x)+str(move.to_value.y)+str(move.team_enum.name)
+            move_list.append(move_str)
+        return str(set(move_list))
 
+    def min_max(self, new_state: GameState, depth: int, alpha, beta, memo : dict = {}, end : bool = False) -> list[int,dict] :
+            hash_list = self.move_hash(new_state)
+            value = self.evaluate(new_state)
+            if hash_list in memo:
+                #logging.info("!!!!!!!!!!!!!!!!!!!!!!")
+                if end:
+                    return [memo[hash_list], memo]
+                return memo[hash_list]
+            #logging.info(str(self.move_hash(new_state)))
             if depth == 0 or new_state.current_team == None:
-                return self.evaluate(new_state) #  value berechnen
+                return value #  value berechnen
             maximizing = (new_state.current_team.name == self.game_state.current_team.name)
             
             #logging.info("Maximizing: "+ str(maximizing) +";  "+ str(new_state.current_team.name) +"|"+ str(self.game_state.current_team.name))#,"| Teams :", new_state.current_team, self.game_state.current_team)
@@ -20,29 +36,37 @@ class Logic(IClientHandler):
             #logging.info(str(new_state.board.pretty_print()))
             if maximizing:
                 if new_state.possible_moves == []:
-                    return self.evaluate(new_state)
+                    if end:
+                        return [value, memo]
+                    return value
                 maxEval = -100 # replacement (- inf)
                 for child in new_state.possible_moves:
-                    eval = self.min_max(new_state.perform_move(child), depth - 1, alpha, beta, child)
+                    eval = self.min_max(new_state.perform_move(child), depth - 1, alpha, beta, memo, False)
                     maxEval = max(maxEval, eval)
                     alpha = max(alpha, eval)
                     if beta <= alpha:
                         break
                 memo[hash_list] = maxEval
-                return maxEval
+                if end:
+                    return [maxEval, memo]
+                else:
+                    return maxEval
             
             else:
                 minEval = 100 # replacement (- inf)
                 for child in new_state.possible_moves:
-                    eval = self.min_max(new_state.perform_move(child), depth - 1, alpha, beta, child)
+                    eval = self.min_max(new_state.perform_move(child), depth - 1, alpha, beta, memo , False)
                     minEval = min(minEval, eval)
                     beta = min(beta, eval)
                     if beta <= alpha:
                         break
                 memo[hash_list] = minEval
-                return minEval
+                if end:
+                    return [minEval, memo]
+                else:
+                    return minEval
     
-    def evaluate(self, state: GameState):
+    def evaluate(self, state: GameState) -> int:
         if (self.game_state.current_team.name == self.game_state.first_team.name):
             max = state.first_team.fish
             min = state.second_team.fish
@@ -55,23 +79,31 @@ class Logic(IClientHandler):
         max_move = None
         max_val = -1000
         # actual min_max:
-        ''''       
+        xmemo : dict = {}
         for child in self.game_state.possible_moves:
-            val = self.min_max(self.game_state, 3, True, max_val, 100, child)
-            print(val, child)
+            if len(self.game_state.board.get_empty_fields()) > 48:
+                mini_max = self.min_max(self.game_state.perform_move(child), 6, max_val, 100, xmemo, True)
+            elif len(self.game_state.board.get_empty_fields()) > 39:
+                mini_max = self.min_max(self.game_state.perform_move(child), 3, max_val, 100, xmemo, True)
+            elif len(self.game_state.board.get_empty_fields()) > 33:
+                mini_max = self.min_max(self.game_state.perform_move(child), 2, max_val, 100, xmemo, True)
+            else:
+                mini_max = self.min_max(self.game_state.perform_move(child), 1, max_val, 100, xmemo, True)
+            val = mini_max[0]
+            xmemo.update(mini_max[1])
             if val > max_val:
                 max_move = child
                 max_val = val
-        '''
-        child = self.game_state.possible_moves[random.randint(0, len(self.game_state.possible_moves)-1)]
-        val = self.min_max(self.game_state, 2, max_val, 100, child)
+        '''child = self.game_state.possible_moves[random.randint(0, len(self.game_state.possible_moves)-1)]
+        val = self.min_max(self.game_state, 2, max_val, 100, {})
         #logging.info(val, child)
         if val > max_val:
             max_move = child
-            max_val = val
+            max_val = val'''
         return max_move
     
     def calculate_move(self) -> Move:
+
         if len(self.game_state.possible_moves) == 1:
             return self.game_state.possible_moves[0]
         if self.game_state.turn < 8:
