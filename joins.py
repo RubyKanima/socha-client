242107c7d44d7212ad150cb55511394a7f3b9927
Tim Te <100081870+RubyKanima@users.noreply.github.com>
2023-04-27 18:58:15 +0200

    v7.1.0

--- joins.py
+++ joins.py
@@ -1,20 +1,94 @@
 import logging
 
 class Joins():
+    '''
+    the `Joins` class contains a series of function for joining two lists which are not tupels
 
-    def left_join(left, right) -> list:
+    all basic joins compare the `items` from the left list and right list
+    - Parameters:
+        - `left`
+        - `right`
+
+    all on joins compare the `attributes` from each `item` from left and right
+    - Parameters:
+        - `left`
+        - `right`
+        - `attr`
+        - `only_attr`:bool
+    '''
+
+    # join
+
+    def left_outer_join(left: any, right: any) -> list:
+        '''
+        left_outer_join contains all items from the left list which are not in the right list
+
+        `if left[item] != right[item]` -> `[left[item]]`
+        '''
         return [each for each in left if each not in right]
 
-    def right_join(left, right) -> list:
-        return [each for each in right if each not in left]
+    def right_outer_join(left: any, right: any) -> list:
+        '''
+        right_outer_join contains all items from the left list which are not in the right list
 
-    def inner_join(left, right) -> list:
+        `if right[item] != left[item]` -> `[right[item]]`
+        '''
+        return [each for each in right if each not in left]
+    
+    def inner_join(left: any, right: any) -> list:
+        '''
+        inner_join contains all items from the left list which are in the right list
+
+        `if left[item] == right[item]` -> [left[item] or right[item]]
+        '''
         return [each for each in left if each in right]
     
-    def outer_join(left, right) -> list:
-        return Joins.left_join(left,right).extend(Joins.right_join(left, right))
+    def outer_join(left: any, right: any) -> list:
+        '''
+        outer_join contains all items from the left list which are not in the right list and all items from the right list which are not in the left list
+
+        `left_outer_join + right_outer_join`
+        '''
+        return Joins.left_outer_join(left,right).extend(Joins.right_outer_join(left, right))
     
-    def inner_join_on(left, right, attr, only_attr = False) -> list:
+    # join on
+
+    def left_outer_join_on(left: any, right: any, attr, only_attr = False) -> list:
+        ''' missing code '''
+
+    def right_outer_join_on(left: any, right: any, attr, only_attr = False) -> list:
+        ''' missing code '''
+
+    def left_join_on(left: any, right: any, attr, only_attr = False) -> list:
+        ''' missing code'''    
+
+    def right_join_on(left: any, right: any, attr, only_attr = False) -> list:
+        ''' missing code'''
+
+    def left_inner_join_on(left: any, right: any, attr, only_attr = False):
+        '''
+        left_inner_join returns the left values of an inner_join_on
+
+        `if left_item[attribute] == right_item[attribute]` -> `[left_item]` or `[left_item[attribute]]`
+        '''
+
+        return [each[0] for each in Joins.inner_join_on(left, right, attr, only_attr)]
+    
+    def right_inner_join_on(left: any, right: any, attr, only_attr = False):
+        '''
+        right_inner_join_on  returns the right values of an inner_join_on
+
+        `if left_item[attribute] == right_item[attribute]` -> `[right_item]`or `[right_item[attribute]]`
+        '''
+
+        return [each[1] for each in Joins.inner_join_on(left, right, attr, only_attr)]
+
+    def inner_join_on(left: any, right: any, attr, only_attr = False) -> list:
+        '''
+        inner_join_on returns a list of all items from a left list whose item's attribute equals to the attribute of right list's item
+
+        `if left_item[attribute] == right_item[attribute]` -> `[[left_item],[right_item]]` or `[attribute]`
+        '''
         if not isinstance(left, type(right)):
             logging.error(f"TypeError: {type(left)} does not equal {type(right)}")
             raise TypeError
@@ -25,7 +99,7 @@ class Joins():
                 raise TypeError
             if only_attr:
                 if _type == list | dict:
-                    return [getattr(each, attr) for each in left for other in right if each[attr] == other[attr]]
+                    return [each[attr] for each in left for other in right if each[attr] == other[attr]]
                 return [getattr(each, attr) for each in left for other in right if getattr(each, attr) == getattr(other, attr)]
             else:
                 if _type == list | dict:
@@ -35,3 +109,12 @@ class Joins():
             logging.error(f"Attribute {attr} is not in {left} or {right}")
         except ValueError:
             logging.error(f"{attr} does not fit {_type}")
+
+    def outer_join_on(left: any, right: any , attr, only_attr = False) -> list:
+        ''' missing code'''
+
+        '''
+        outer_join_on returns a list of all outer_left_join_on and outer_right_join_on items
+
+        `outer_left_join_on + outer_right_join_on`
+        '''
--- logic.py
+++ logic.py
@@ -1,33 +1,164 @@
 from socha import *
-import logging
+from socha_extentions import *
 from joins import Joins
-from alpha_beta import Alpha_Beta
-from socha_extentions import CustomBoard, Tile, Blob
+from typing import List, Optional, Any
+#from tabulate import tabulate
+import logging
+import random
 
 class Logic(IClientHandler):
-    def __init__(self):
+    def __init__(self):     
         self.game_state: GameState
         self.all_fields: list[Field]
-        self.opp_possible_moves: list[Move]
-        self.inters: list[list[Move, Move]]
+        self.other_possible_moves: list[Move]
+        self.full_inters: list[list[Move, Move]]
+        self.left_inters: list[Move]
         self.inters_to: list
 
     def on_update(self, state: GameState):
         self.game_state = state
         self.all_fields = self.game_state.board.get_all_fields()
-        self.opp_possible_moves = self.game_state._get_possible_moves(self.game_state.current_team.opponent)
+        if not self.game_state.current_team == None:
+            if not self.game_state.current_team.opponent == None:
+                self.other_possible_moves = self.game_state._get_possible_moves(self.game_state.current_team.opponent)
         self.game_state.board.get_all_fields()
-        self.inters = Joins.inner_join_on(self.game_state.possible_moves, self.opp_possible_moves, "to_value", False)
-        self.inters_to = Joins.inner_join_on(self.game_state.possible_moves, self.opp_possible_moves, "to_value", True)
+        if not self.other_possible_moves == [] and not self.game_state.possible_moves == []:
+            self.full_inters = Joins.inner_join_on(self.game_state.possible_moves, self.other_possible_moves, "to_value", False)
+            self.left_inters = [each[0] for each in self.full_inters]
+            self.inters_to = Joins.inner_join_on(self.game_state.possible_moves, self.other_possible_moves, "to_value", True)
 
     def calculate_move(self) -> Move:
-        logging.info("test")
-        logging.info(Joins.inner_join_on(self.game_state.possible_moves, self.opp_possible_moves, "to_value", True))
-        Board.pretty_print(CustomBoard.del_fields(self.game_state.board))
-        return self.game_state.possible_moves[0]
+        if self.game_state.turn < 4:                            # Beginning Moves
+            logging.info("most_possible_move")
+            return Alpha_Beta.get_most_possible_move(self)
+        if not self.inters_to == []:                            # Following Moves against enemy
+            logging.info("get_alpha_beta_cut_move")
+            return Alpha_Beta.get_alpha_beta_cut_move(self)
+        if self.inters_to == []:                                # Following Moves if no moves possible against enemies
+            logging.info("get_most_possible_move")
+            return Alpha_Beta.get_most_possible_move(self)
+        if not self.other_possible_moves:                       # Following Moves if the enemies don't matter
+            logging.info("get_most_possible_move")
+            return Alpha_Beta.get_most_possible_move(self)
+            '''
+            logging.info("get_alpha_beta_fish_move")
+            return Alpha_Beta.get_alpha_beta_fish_move(self)
+            '''
+            '''
+            logging.info("_get_depth_move")
+            return Tree._get_depth_move(self)
+            '''
+        if not self.game_state.possible_moves == []:            # Following Moves if he is clueles
+            return random.choice(self.game_state.possible_moves)
+        else:                                                   # No Move possible
+            return None
+        
+        '''
+        # isnt working properly
+        if not self.other_possible_moves:
+            return self.max_move(self.game_state.possible_moves, Tree._depth) #should work
+        if self.inters_to == []:
+            # return self.max_move(self.game_state.possible_moves, Alpha_Beta.alpha_beta_fish, 3, ) # needs check
+            return self.max_move(Joins.inner_join_on, self.game_state, Alpha_Beta.alpha_beta, self)
+        if self.game_state.turn > 8 and not self.inters_to == []:
+            return self.max_move(self.inters, Intersection.delta_possibles)
+        else:
+            return random.choice(self.game_state.possible_moves)
+            '''
+        
+    def max_move(list_f, state: GameState, evaluate_f, logic: Optional[Any] = None, depth: Optional[int] = None, extra_f : Optional[Any] = None):
+        '''
+        is malfunctioning because of the given attributes and incompatible Logic !
+        '''
+        
+        max_val: int = 0
+        max_move: Move
+        _list: List[Move] = list_f(state)
+        move_list: List[Move] = _list if type(list_f) is not list else list_f #dangerous
+        
+        if evaluate_f is Alpha_Beta.alpha_beta:
+            for move in move_list:
+                val: int = evaluate_f(logic, state.perform_move(move), depth, list_f, extra_f)
+                if val > max_val:
+                    max_move = move
+                    max_val = val
 
-    def depth_search():
-        return "missing function"
+        else:
+            for move in move_list:
+                val: int = evaluate_f(state.perform_move(move))
+                if val > max_val:
+                    max_move = move
+                    max_val = val
+        return max_move
+    
+    def get_possible_movements(state: GameState, team: TeamEnum = None):
+        if team == None:
+            team = state.current_team
+        possible_movements = []
+        penguins: list[Penguin] = state.board.get_teams_penguins(team.name)
+        #logging.info(f"\n team, penguins: {team, penguins} \n")
+        for penguin in penguins:
+            possible_movements.extend(state.board.possible_moves_from(penguin.coordinate, penguin.team_enum))
+        return possible_movements
+    
+    def get_fields_in_direction(board: Board, origin: HexCoordinate, direction: Vector, team_enum: Optional[TeamEnum] = None) -> List[Field]:
+        """
+        Gets all moves in the given direction from the given origin.
 
+        Args:
+            origin: The origin of the move.
+            direction: The direction of the move.
+            team_enum: Team to make moves for.
+
+        Returns:
+                List[Field]: List of moves that can be made in the given direction from the given index,
+                            for the given team_enum
+        """
+        if team_enum is None:
+            team_enum = board.get_field(origin).penguin.team_enum
+        if not board.get_field(origin).penguin or board.get_field(origin).penguin.team_enum != team_enum:
+            return []
+
+        fields = []
+        for i in range(1, board.width()):
+            destination = origin.add_vector(direction.scalar_product(i))
+            if board._is_destination_valid(destination):
+                fields.append(board.get_field(destination))
+            else:
+                break
+        return fields
+    
+    def get_possible_fields_from(state: GameState, position: HexCoordinate, team_enum: TeamEnum = None)-> List[Field]:
+        if team_enum == None:
+            team_enum = state.current_team.name
+        if not state.board.is_valid(position):
+            raise IndexError(f"Index out of range: [x={position.x}, y={position.y}]")
+        if not state.board.get_field(position).penguin or (
+                team_enum and state.board.get_field(position).penguin.team_enum != team_enum):
+            return []
+        return [field for direction in Vector().directions for field in
+                Logic.get_fields_in_direction(state.board, position, direction, team_enum)]
+
+    def get_possible_fields(state: GameState, team: TeamEnum = None)-> List[Field]:
+        if team == None:
+            team = state.current_team
+        possible_fields = []
+        penguins: list[Penguin] = state.board.get_teams_penguins(team.name)
+        #logging.info(f"\n team, penguins: {team, penguins} \n")
+        for penguin in penguins:
+            possible_fields.extend(Logic.get_possible_fields_from(state, penguin.coordinate, penguin.team_enum))
+        return possible_fields
+"""
+    def print_moves(list: List[Move]):
+        table = [["Team", "From Cart.", "To Cart."]]
+        for each in list:
+            if each.from_value:
+                from_string = str(each.from_value.to_cartesian().x)+", "+str(each.from_value.to_cartesian().y)
+            else:
+                from_string = "None"
+            to_string = str(each.to_value.to_cartesian().x)+", "+str(each.to_value.to_cartesian().y)
+            table.append([str(each.team_enum.name),from_string, to_string])
+        logging.info("\n" + tabulate(table, headers="firstrow", tablefmt="fancy_grid"))
+"""
 if __name__ == "__main__":
     Starter(Logic())
