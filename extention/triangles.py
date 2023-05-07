2ac39ddce3cef94c27b0bbecfec85d13f87be3f8
RubyKanima <ttriebe05@gmail.com>
2023-05-07 23:19:42 +0200

    v7.4.0 PRE 0.0.1
    Triangle modified

--- extention/triangles.py
+++ extention/triangles.py
@@ -1,15 +1,45 @@
 from socha import Field
 
-from dataclasses import dataclass
+from dataclasses import dataclass, field
 
-@dataclass
+@dataclass(eq=False, order=True) # adds __eq__ function
 class Triangle:
     root: Field
     left: Field
     right: Field
-    upside: bool
+    orient: int = field(default=1)
 
     @property
     def fish(self):
-        return sum(self.root.fish, self.left.fish, self.right.fish)
-     
+        return sum(self.root.fish, self.left.fish or 0, self.right.fish or 0)
+
+    def __feq__(self, other: 'Triangle') -> bool:
+        if not isinstance(self, other):
+            return TypeError
+        return self.fish == other.fish
+    
+    def __fgt__(self, other: 'Triangle') -> bool:
+        if not isinstance(self, other):
+            return TypeError
+        return self.fish > other.fish
+
+    def __flt__(self, other: 'Triangle') -> bool:
+        if not isinstance(self, other):
+            return TypeError
+        return self.fish < other.fish
+    
+    def __fge__(self, other: 'Triangle') -> bool:
+        if not isinstance(self, other):
+            return TypeError
+        return self.fish >= other.fish
+    
+    def __fle__(self, other: 'Triangle') -> bool:
+        if not isinstance(self, other):
+            return TypeError
+        return self.fish <= other.fish
+
+    def __roe__(self, other: Field) -> bool:
+        if not isinstance(self.root, other):
+            return TypeError
+        return self.root.coordinate == other.coordinate
+        
