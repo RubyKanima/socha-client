3744a1638aaf2ea52681c1aa747322c0d5ec61e2
YoEnte <67060214+YoEnte@users.noreply.github.com>
2023-05-07 23:25:22 +0200

    v7.0.4 PRE 0.0.2
    triangles by ente

--- extention/triangles.py
+++ extention/triangles.py
@@ -1,17 +1,28 @@
 from socha import Field
 
-from dataclasses import dataclass
+from dataclasses import dataclass, field
 
-@dataclass
-class Triangle:
+@dataclass(order=True)
+class Shape:
     root: Field
+    orient: int = field(default=1)
+
+    def __hash__(self):
+        return str(self.root.coordinate.x) + str(self.root.coordinate.y)
+
+@dataclass(order=True, eq=False)
+class Triangle(Shape):
     left: Field
     right: Field
-    orient: int # 1 or -1
 
     @property
     def fish(self):
         return sum(self.root.fish, self.left.fish, self.right.fish)
      
+@dataclass(order=True, eq=False)
+class Line(Shape):
+    buddy: Field
 
-
+@dataclass(order=True)
+class Group:
+    childs: dict
