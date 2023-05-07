6dcba76fd0e6fac4cd8409da39e1dba6b773cf1f
RubyKanima <ttriebe05@gmail.com>
2023-05-07 23:21:39 +0200

    v7.4.0 PRE 0.0.1
    triangles reupload

--- extention/triangles.py
+++ extention/triangles.py
@@ -7,9 +7,50 @@ class Triangle:
     root: Field
     left: Field
     right: Field
+<<<<<<< HEAD
     orient: int = field(default=1)
+
+    @property
+    def fish(self):
+        return sum(self.root.fish, self.left.fish or 0, self.right.fish or 0)
+
+    def __feq__(self, other: 'Triangle') -> bool:
+        if not isinstance(self, other):
+            return TypeError
+        return self.fish == other.fish
+    
+    def __fgt__(self, other: 'Triangle') -> bool:
+        if not isinstance(self, other):
+            return TypeError
+        return self.fish > other.fish
+
+    def __flt__(self, other: 'Triangle') -> bool:
+        if not isinstance(self, other):
+            return TypeError
+        return self.fish < other.fish
+    
+    def __fge__(self, other: 'Triangle') -> bool:
+        if not isinstance(self, other):
+            return TypeError
+        return self.fish >= other.fish
+    
+    def __fle__(self, other: 'Triangle') -> bool:
+        if not isinstance(self, other):
+            return TypeError
+        return self.fish <= other.fish
+
+    def __roe__(self, other: Field) -> bool:
+        if not isinstance(self.root, other):
+            return TypeError
+        return self.root.coordinate == other.coordinate
+        
+=======
+    orient: int # 1 or -1
 
     @property
     def fish(self):
         return sum(self.root.fish, self.left.fish, self.right.fish)
      
+
+
+>>>>>>> fe6fbe5a2cebbb98d58ee2b02cd61d64d79e0086
