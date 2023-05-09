0bc5a8d1a6c4b0d033f90480bf49292734290d4e
YoEnte <67060214+YoEnte@users.noreply.github.com>
2023-05-09 16:07:52 +0200

    replaced logicEnte with logicEnte2

--- logicEnte.py
+++ logicEnte.py
@@ -1,61 +1,67 @@
 from socha import *
-
-import logging
 import random
 
-from extention import *
-from utils import *
-
 class Logic(IClientHandler):
-    def __init__(self):     
-        self.game_state: GameState
-        self.all_fields: list[Field]
-        self.other_possible_moves: list[Move]
-        #self.full_inters: list[list[Move, Move]]
-        #self.left_inters: list[Move]
-        self.inters_to: list
 
-    def on_update(self, state: GameState):
-        self.game_state = state
-        self.all_fields = self.game_state.board.get_all_fields()
-        if not self.game_state.current_team == None:
-            if not self.game_state.current_team.opponent == None:
-                self.other_possible_moves = self.game_state._get_possible_moves(self.game_state.current_team.opponent)
-        self.game_state.board.get_all_fields()
-        if not self.other_possible_moves == [] and not self.game_state.possible_moves == []:
-            #self.full_inters = Joins.inner_join_on(self.game_state.possible_moves, self.other_possible_moves, "to_value", False)
-            #self.left_inters = [each[0] for each in self.full_inters]
-            self.inters_to = Joins.inner_join_on(self.game_state.possible_moves, self.other_possible_moves, "to_value", True)
+    # //////////////// standard logic ////////////////
 
-    def calculate_move(self):
-        self.poss_moves = self.game_state.possible_moves
-        self.all_fields = self.game_state.board.get_all_fields()
-        self.my_team = self.game_state.current_team
+    gameState: GameState
+
+    def __init__(self):
+        self.my_last_move = None
+        self.enemy_copy_rate = []
+
+    def calculate_move(self) -> Move:
+        print('\n MY TURN \n')
+        self.poss_moves = self.gameState.possible_moves
+        self.all_fields = self.gameState.board.get_all_fields()
+        self.my_team = self.gameState.current_team
         self.op_team = self.my_team.opponent
 
-        logging.info(self.game_state.turn)
-        if self.game_state.turn < 8:                            # Beginning Moves
-            logging.info("most_possible_move")
-            return AlphaBeta.get_most_possible_move(self)
-        if not self.other_possible_moves:                       # Following Moves if the enemies don't matter
-            logging.info("least")
-            return AlphaBeta.get_least_neighbor_move(self)
-        if not self.inters_to:                            # Following Moves against enemy
-            logging.info("least2")
-            return AlphaBeta.get_least_neighbor_move(self)
-        if self.inters_to:                                # Following Moves if no moves possible against enemies
-            logging.info("get_alpha_beta_cut_move")
-            return self.schneider_02()
-    
-        logging.error("UNAVOIDABLE ERROR")
-        return random.choice(self.game_state.possible_moves)
-    
-    # //////////////// ente ////////////////
+        self.my_move = None
+        self.rand_move = random.choice(self.poss_moves)
+
+        '''
+            PLAN:
+
+            Brücken
+            Felderexisting
+            Loose parts
+            "goldens"
+            field score (score via most possible fish on next fields)
+            most center: go from center out
+            abschneiden (Lucca)
+            copy
+            copy breaker
+        '''
+
+        # if Bot is first:
+        if self.my_team == self.gameState.first_team:
+            pass
+        # if Bot is second:
+        else:
+            pass
+
+        self.my_move = self.schneider_02()
+
+        # self.my_last_move == None: random, else use:
+        self.my_move = self.my_move if self.my_move != None else self.get_least_neighbor_move()
+        self.my_last_move = self.my_move
+        return self.my_move
+
+    def on_update(self, state: GameState):
+        self.gameState = state
+
+    #def while_disconnected(self, player_client):
+    #    player_client.connect()
+    #    player_client.join_game()
+
+    # //////////////// tactics ////////////////
 
     def schneider_02(self) -> Move:
 
         # if first match's turn:
-        if self.game_state.turn < 8: 
+        if self.gameState.turn < 8: 
             moves1 = []
             for p in self.poss_moves:
                 moves1.append({
@@ -117,9 +123,9 @@ class Logic(IClientHandler):
             for v in Vector().directions:
             
                 moves = []
-                for i in range(1, self.game_state.board.width()):
+                for i in range(1, self.gameState.board.width()):
                     destination: HexCoordinate = p.coordinate.add_vector(v.scalar_product(i))
-                    if self.game_state.board._is_destination_valid(destination):
+                    if self.gameState.board._is_destination_valid(destination):
 
                         move = Move(team_enum=team.name, from_value=p.coordinate, to_value=destination)
                         vector = Vector(move.to_value.x - move.from_value.x, move.to_value.y - move.from_value.y)
@@ -150,9 +156,62 @@ class Logic(IClientHandler):
                 poss_moves.extend(moves)
 
         return poss_moves
+
+        '''
+        get penguins
+        get possible moves from each p 
+        -> [Move, to, Vector, direction Vector, list[Field] between from and to, list[Field] after to]
+        '''
+
+    def get_least_neighbor_move(logic: 'Logic'):
+        min_val = 10
+        max_move = logic.gameState.possible_moves[0]
+
+        for move in logic.gameState.possible_moves:
+            #state = logic.game_state.perform_move(move)
+            valid_n = []
+            for each in move.to_value.get_neighbors(): 
+                if logic.gameState.board._is_destination_valid(each):
+                    valid_n.append(each)
+            val = len(valid_n)
+            #logging.info(str(val)+ str(valid_n))
+            if val <= min_val and not val == 0:
+                min_val = val
+                max_move = move
+        return max_move
+
+    # //////////////// tools ////////////////
+
+    # same function as in socha api, but with possibility for other gamestate than "self.gameState":
+    def own_pretty_print(self, gameState: GameState):
+        print()
+        for i, row in enumerate(gameState.board.board):
+            if (i + 1) % 2 == 0:
+                print(" ", end="")
+            for field in row:
+                if field.is_empty():
+                    print("~", end=" ")
+                elif field.is_occupied():
+                    print(field.get_team().value[0], end=" ")
+                else:
+                    print(field.get_fish(), end=" ")
+            print()
+        print()
         
-logic = Logic()
-
 if __name__ == "__main__":
-    Starter(logic)
+    Starter(Logic())
 
+# NOTIZEN:
+
+# detect brigdes:
+# neigbohur fields -> most fields with whitespace
+
+# best score + shortest path if multiple can reach -> else stacking myb
+# but if longest path -> can open new possibilities, come out of corners???
+
+# optimize field score: not score all each turn, but edit that ones, that are in line of made move
+
+# optimize field score 2: not score all fields, but only these, where move can be performed from
+# bug: recursion does not look at "removed paths"
+
+# optimize field score 3: Whiteboard
