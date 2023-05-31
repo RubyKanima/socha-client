4db33b0fa70398e5477a02f9ee7b6305c5d0745c
YoEnte <67060214+YoEnte@users.noreply.github.com>
2023-06-01 00:17:36 +0200

    ai prepare

--- ai_logic.py
+++ ai_logic.py
@@ -35,32 +35,90 @@ class Logic(IClientHandler):
         self.tri_board.__own_repr__()
         logging.info(self.game_state.turn)
 
-        for t in self.tri_board.tri_board:
-            print(t)
-            print("1", 1)
 
-        print(len(self.tri_board.tri_board))
-        '''
-        white 0/1
-        yellow 0/1
-        red 0/1
-        black 0/1
-        fish linear(1, 4, x)
-        group fish sigmoid(1, 80, x)
-        score/average score linear(1, 4, x)
-        tri inters linear(1, 6, x)
-        enemy inters linear(0, 4, x)
-        enemy behind sigmoid(0, 12, x)
-        enemy between sigmoid(0, 12, x)
-        mate inters linear(0, 3, x)
-        mate behind sigmoid(0, 12, x)
-        mate between sigmoid(0, 12, x)
+        # yes
 
-        instead of plugging fish values into function, plug average fish values
-        '''
+        # all fish from all groups
+        all_fish = 0
+        for g in self.tri_board._groups:
+            all_fish += g.fish
+
+        # inters
+        inters_from_own = inters_for_ai(self.game_state, self.game_state.current_team.name)
+        inters_from_op = inters_for_ai(self.game_state, self.game_state.current_team.opponent.name)
+
+        # make ai dict
+        for move in self.game_state.possible_moves:
+            
+            tile = self.tri_board.get_tile(move.to_value)
+
+            ai_infos = {
+                "white": 0,
+                "red": 0,
+                "black": 0,
+                "yellow": 0,
+
+                "fish": tile.fish,
+                "fish_mapped": linear_map(0, 4, tile.fish),
+                "fish_group": self.tri_board.groups[tile.group].fish, # value
+                "fish_group_percent": round(self.tri_board.groups[tile.group].fish / all_fish, 3), # percent of board
+                "fish_possible": self.tri_board.get_possible_fish(tile.root),
+
+                "tri_inters": tile.inters,
+                "tri_inters_mapped": linear_map(1, 6, tile.inters),
+
+                "enemy_inters": 0,
+                "enemy_inters_mapped": 0,
+                "enemy_behinds": 0,
+                "enemy_betweens": 0,
+                "enemy_own_behind": 0,
+                "enemy_own_between": 0,
+
+                #"mate_inters": 0,
+                #"mate_inters_mapped": 0,
+                #"mate_behinds": 0,
+                #"mate_betweens": 0,
+                #"mate_own_behind": 0,
+                #"mate_own_between": 0,
+            }
+
+            ai_infos[tile.spot] = 1
+
+            print(move.to_value.x, move.to_value.y, ai_infos)
+
+            # ai for each field here
+            # evaluate best field
+
+        # take best here
+        # and eval your neural network
         
         return random.choice(self.game_state.possible_moves)
         
 if __name__ == "__main__":
     Starter(Logic())
 
+
+def inters_for_ai(state: GameState, team: TeamEnum) -> list[dict]:
+    
+    pm_from_peng_op = get_possible_fields(state, team)
+
+    intersections = {}
+    for p in state.board.get_teams_penguins(team):
+        for v in Vector().directions:
+            inters_for_dir = {}
+            for i in range(1, 8):
+                destination: HexCoordinate = p.coordinate.add_vector(v.scalar_product(i))
+                if own_is_valid(destination) and state.board.get_field(destination).fish > 0:
+                    
+                    for inter in inters_for_dir:
+                        inters_for_dir[inter]["behind"] += 1
+
+                    if state.board.get_field(destination) in pm_from_peng_op:
+                        inters_for_dir[hash(destination)] = {"between": i - 1, "behind": 0}
+                else:
+                    break
+
+    return intersections
+
+def hash(coordinate: HexCoordinate):
+    return (str(coordinate.x) + str(coordinate.y))
--- utils/ai_utils.py
+++ utils/ai_utils.py
@@ -1,5 +1,3 @@
-def linear_map(max, value):
-    return round(min(1, value / max), 3)
-
-def sigmoid_map(max, value):
-    return 1
+def linear_map(min_v, max_v, value, min_map = 0, max_map = 1):
+    map_v = (value - min_v) / (max_v - min_v) * (max_map - min_map) + min_map
+    return 1 if map_v > 1 else 0 if map_v < 0 else map_v
